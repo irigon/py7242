33977e37e737a24f6e938f71688b63177ca49d41
Jose Irigon de Irigon <jose.irigon@tu-dresden.de>
2018-03-05 15:39:40 +0100

    preparing env for travis & coveralls

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='libs',
+      version='1.0',
+      description='libraries used in py5050',
+      author='Jose Irigon',
+      author_email='irigon@gmail.com',
+      packages=['libs'],
+     )
